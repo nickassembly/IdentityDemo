c092c717bec2c69fcac3d89fba3894f0f4d0da3b
nickassembly <nickassembly@github.com>
2018-07-23 08:53:00 -0500

    Configure startup with password and cookie middleware

--- IdentityDemo/Startup.cs
+++ IdentityDemo/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace IdentityDemo
 {
@@ -29,6 +30,37 @@ namespace IdentityDemo
          services.AddIdentity<ApplicationUser, IdentityRole>()
              .AddEntityFrameworkStores<ApplicationDbContext>()
              .AddDefaultTokenProviders();
+
+         services.Configure<IdentityOptions>(options =>
+         {
+            // Password settings
+            options.Password.RequireDigit = true;
+            options.Password.RequiredLength = 8;
+            options.Password.RequireNonAlphanumeric = false;
+            options.Password.RequireUppercase = true;
+            options.Password.RequireLowercase = false;
+            options.Password.RequiredUniqueChars = 6;
+
+            // Lockout settings
+            options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(30);
+            options.Lockout.MaxFailedAccessAttempts = 10;
+            options.Lockout.AllowedForNewUsers = true;
+
+            // User settings
+            options.User.RequireUniqueEmail = true;
+         });
+
+         services.ConfigureApplicationCookie(options =>
+         {
+            // Cookie settings.
+            options.Cookie.HttpOnly = true;
+            options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+            // If the LoginPath isn't set, Asp.Net Core defaults path to /Account/Login.
+            options.LoginPath = "/Account/Login";
+            // If the AccessDeniedPath isn't set, Asp.Net Core defaults to /Account/AccessDenied.
+            options.AccessDeniedPath = "/Account/AccessDenied";
+            options.SlidingExpiration = true;
+         });
 
          // Add application services.
          services.AddTransient<IEmailSender, EmailSender>();
